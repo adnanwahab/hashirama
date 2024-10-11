0ca44202cbccaa26c82cbdedb83443412683610e
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-11 07:59:05 -0700

    simplify

--- infra/scripts/restart-blog.sh
+++ infra/scripts/restart-blog.sh
@@ -4,11 +4,11 @@ sudo caddy reload --config /etc/caddy/Caddyfile
 
 sudo systemctl daemon-reload
 
-sudo systemctl stop homelab-status-page
+# sudo systemctl stop homelab-status-page
 
-sudo systemctl start homelab-status-page
+# sudo systemctl start homelab-status-page
 
-sudo systemctl enable homelab-status-page
+# sudo systemctl enable homelab-status-page
 
 sudo  systemctl stop caddy
 
@@ -16,5 +16,5 @@ sudo systemctl start caddy
 
 sudo systemctl enable caddy 
 
-sudo systemctl status caddy & sudo systemctl status homelab-status-page
-mamba activate sam
+sudo systemctl status caddy #& sudo systemctl status homelab-status-page
+#mamba activate sam
