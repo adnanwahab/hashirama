06a055f13587fbc2f91a72a7df9b9b4ecce65c83
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-12 19:16:34 -0700

    simplify

--- views/odyssey/Dynamichow.tsx
+++ views/odyssey/Dynamichow.tsx
@@ -11,9 +11,8 @@ console.log("returning iframe", src)
  //src = "https://observablehq.com/embed/@roboticsuniversity/alan-how?cell=*"
   return (
     <iframe
-      style={{ backgroundColor: 'white' }}
  
-      className="h-80 object-cover object-left w-full"
+      className="border border-white/10 h-80 object-cover object-left w-full"
       src={src}
     ></iframe>
   );
@@ -37,52 +36,26 @@ function DynamicHow() {
                 <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                
                 </div>
-                <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                
                 <TeleGuidanceFrame link={second_bento[0]}/>
 
-                  {/* <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                  </div> */}
-                </div>
+               
+                
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
             </div>
-            {/* <div className="relative max-lg:row-start-1">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-
-                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
-                <TeleGuidanceFrame link={second_bento[1]}/>
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
-            </div>
-            <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-              <div className="absolute inset-px rounded-lg bg-white"></div>
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
-                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-      
-                </div>
-                <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
-                <TeleGuidanceFrame link={second_bento[0]}/>
-
-                </div>
-              </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
-            </div> */}
+ 
             <div className="relative lg:row-span-2">
               <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-     
-           
-                </div>
-                <div className="relative min-h-[30rem] w-full grow">
-                  <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+        
+                
+                  
                   <TeleGuidanceFrame link={second_bento[0]}/>
 
 
-                  </div>
-                </div>
+                  
+                
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
             </div>
