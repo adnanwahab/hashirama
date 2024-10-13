e710f409a294df1ae32658fea3b39d498fb68497
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-13 12:54:45 -0700

    simplify

--- infra/scripts/course_gen.sh
+++ infra/scripts/course_gen.sh
@@ -2,6 +2,6 @@
 # makes a data app for each one - in the
 
 # Check if the JSON path is correct and the file is not empty
-
+rm course_content/docs/*.md
 
 python3 infra/scripts/course_gen_step_2.py --query "gen a markdown of example markdown about the robotics topic " --input_dir data/robotics-odyssey/  --output_dir=js/course_content/docs/
