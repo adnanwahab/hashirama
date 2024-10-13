77e4d7ea5c08ec4a299d7a8a6b791f09f8b00d1d
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-13 13:23:28 -0700

    simplify

--- infra/scripts/course_gen_step_2.py
+++ infra/scripts/course_gen_step_2.py
@@ -85,7 +85,7 @@ def empty_file(output_dir):
 def parse_gpt(results):
     comp = results
     if len(comp.choices) == 0:
-        return print('No completion available')
+        return None
 
     message_content = comp.choices[0].message.content
     return message_content
@@ -127,6 +127,7 @@ def process_chunk(query, filename, content, index):
 # is jupyter a thing? try collarobaroty - add currsor - to obs+jpy - (LLM_prediciton_planning, cgi, hardware) 
 # add in dict types from fastapi - 
 def process_all_files_in_directory(query, directory_path):
+    print('processing all files in directory', directory_path)
     files = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path) if filename.endswith('.md')]
     file_contents = [open(file_path, 'r').read() for file_path in files] 
     file_dict = {file_path: content for file_path, content in zip(files, file_contents)}
@@ -182,8 +183,8 @@ import re
 
 # Example usage
 json_file_path = 'data/robotics-odyssey/modules.json'
-input_dir = 'data/robotics-odyssey/'
-output_dir = 'course_content/docs'
+input_dir = 'course_content/src'
+output_dir = 'course_content/src'
 
 
 
