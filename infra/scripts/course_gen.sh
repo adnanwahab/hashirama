086d89456be8e4bce592bead88ab8db7a1c172b7
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-13 16:31:18 -0700

    simplify

--- infra/scripts/course_gen.sh
+++ infra/scripts/course_gen.sh
@@ -12,7 +12,7 @@
 
 
 #file_names=$(jq -r '.modules[].title | gsub(" "; "-") | ascii_downcase | gsub("[^a-z0-9-]"; "") + ".md"' data/robotics-odyssey/modules.json)
-# submodule_file_names=$(jq -r '.modules[].sub_modules[] | gsub(" "; "-") | ascii_downcase | gsub("[^a-z0-9-]"; "") + ".md"' data/robotics-odyssey/modules.json)
+submodule_file_names=$(jq -r '.modules[].sub_modules[] | gsub(" "; "-") | ascii_downcase | gsub("[^a-z0-9-]"; "")' data/odyssey/modules.json)
 # echo $submodule_file_names
 
 # #echo $file_names
@@ -20,13 +20,13 @@
 for filename in $submodule_file_names; do  # Indentation fixed here
     echo "Processing $filename"  # Add a command inside the loop
     # Add your desired command here, for example:
-    mkdir -p "course_content/src/$filename"
-    echo "---" > "course_content/src/$filename"
-    echo "title: $filename" >> "course_content/src/$filename"
-    echo "toc: true" >> "course_content/src/$filename"
-    echo "---" >> "course_content/src/$filename"
-    done
-#python3 infra/scripts/course_gen_step_2.py --query "gen a markdown of example markdown about the robotics topic " --input_dir data/odyssey/  --output_dir=data/odyssey/
+    #mkdir -p "data/odyssey/$filename"
+    echo "---" > "data/odyssey/$filename"
+    echo "title: $filename" >> "data/odyssey/$filename"
+    echo "toc: true" >> "data/odyssey/$filename"
+    echo "---" >> "data/odyssey/$filename"
+done
+python3 infra/scripts/course_gen_step_2.py --query "gen a markdown of example markdown about the robotics topic " --input_dir data/odyssey/  --output_dir=data/odyssey/
 
 
 bun run js/convert_md_to_sqlite.js
