222040d16f5dd335d07fddf05c0f323eade5304e
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-12 17:01:21 -0700

    simplify

--- views/odyssey/robotics-odyssey.tsx
+++ views/odyssey/robotics-odyssey.tsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import Header from './Header';
 
-function TeleGuidanceFrame() {
+const observable_links = {
+  voxels:
+    "https://observablehq.com/embed/@roboticsuniversity/voxels-diffusion-policy-3d?cell=*",
+  //"worrydream": "https://observablehq.com/embed/@roboticsuniversity/worrydream?cell=*",
+  //"dynamicland.org": "https://observablehq.com/embed/@roboticsuniversity/dynamicland.org?cell=*",
+  dynamicland: "https://observablehq.com/embed/@roboticsuniversity/dynamicland?cell=*",
+  "livekit_subscriber": "https://observablehq.com/embed/@roboticsuniversity/livekit?cell=*",
+  //"livekit_subscriber": "https://observablehq.com/embed/@roboticsuniversity/livekit-robotics-tele-guidance?cell=*",
+
+  alan_how:
+    "https://observablehq.com/embed/@roboticsuniversity/alan-how?cell=*",
+  hardware: "https://observablehq.com/embed/@roboticsuniversity/robotics-hardware?cell=*",
+  prediction:
+    "https://observablehq.com/embed/@roboticsuniversity/3-planning-prediction?cell=*",
+  infra:
+    "https://observablehq.com/embed/@roboticsuniversity/infrastructure-notebook@13?cell=*",
+  democracy:
+    "https://observablehq.com/embed/@roboticsuniversity/collaborative-ui-twitch-plays-robot?cell=*",
+  //twitch: TwitchPlaysPokemonPanel,
+
+  //twitch: "https://observablehq.com/embed/@roboticsuniversity/voxels-diffusion-policy-3d?cell=*",
+
+  research:
+    "https://observablehq.com/embed/@roboticsuniversity/5000-research-papers?cell=*",
+  //semseg: "https://observablehq.com/embed/@roboticsuniversity/semantic-segmentation-robot?cell=*",
+};
+
+function TeleGuidanceFrame(props) {
+  let  src = props.link;
+
+ src = "https://observablehq.com/embed/@roboticsuniversity/alan-how?cell=*"
   return (
     <iframe
       style={{ backgroundColor: 'white' }}
  
       className="h-80 object-cover object-left w-full"
-      src="https://observablehq.com/embed/@roboticsuniversity/alan-how?cell=*"
+      src={src}
     ></iframe>
   );
 }
+
+const first_bento = [
+  "livekit_subscriber",
+  "prediction",
+  "hardware",
+  "voxels",
+  
+].map(_ => observable_links[_])
+
+const second_bento = [
+  'dynamicland',
+  'alan_how',
+]
+
+
 
 function RoboticsOdyssey() {
   return (
@@ -20,11 +65,11 @@ function RoboticsOdyssey() {
         <div className="overflow-hidden">
           <main>
 
-            <TeleGuidance />
+            <TeleGuidance list_of_links={first_bento}/>
 
-            <ObservablePreview></ObservablePreview>
+            {/* <ObservablePreview></ObservablePreview> */}
 
-              <TeleGuidance />
+              <TeleGuidance list_of_links={second_bento} />
         
                   <Footer />
           </main>
@@ -325,71 +370,67 @@ return (
 }
 
 
-
-function TeleGuidance() {
+function TeleGuidance({list_of_links}) {
   return (
     <div className="bg-gray-900 py-24 sm:py-32">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-base/7 font-semibold text-indigo-400">Deploy faster</h2>
-        <p className="mt-2 max-w-lg text-pretty text-4xl font-medium tracking-tight text-white sm:text-5xl">
-          Everything you need to deploy your app.
+        {/* <h2 className="text-base font-semibold text-indigo-400">Deploy faster</h2> */}
+        <p className="mt-2 max-w-lg text-4xl font-medium tracking-tight text-white sm:text-5xl">
+          {/* Everything you need to deploy your app. */}
         </p>
         <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-          <div className="flex p-px lg:col-span-4">
-            <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]">
-            <TeleGuidanceFrame />
-
-              <div className="p-10">
-                <h3 className="text-sm/4 font-semibold text-gray-400">Releases</h3>
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-white">Push to deploy</p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida justo et nulla efficitur, maximus
-                  egestas sem pellentesque.
-                </p>
-              </div>
+          <div className="lg:col-span-4 flex flex-col rounded-lg bg-gray-800 overflow-hidden">
+            <div className="flex-grow">
+              <TeleGuidanceFrame link={list_of_links[0]}/>
             </div>
+            {/* <div className="p-10">
+              <h3 className="text-sm font-semibold text-gray-400">Releases</h3>
+              <p className="mt-2 text-lg font-medium tracking-tight text-white">Push to deploy</p>
+              <p className="mt-2 max-w-lg text-sm text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida justo et nulla efficitur, maximus
+                egestas sem pellentesque.
+              </p>
+            </div> */}
           </div>
-          <div className="flex p-px lg:col-span-2">
-            <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-tr-[2rem]">
-            <TeleGuidanceFrame />
-
-              <div className="p-10">
-                <h3 className="text-sm/4 font-semibold text-gray-400">Integrations</h3>
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-white">Connect your favorite tools</p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                  Curabitur auctor, ex quis auctor venenatis, eros arcu rhoncus massa.
-                </p>
-              </div>
+          <div className="lg:col-span-2 flex flex-col rounded-lg bg-gray-800 overflow-hidden">
+            <div className="flex-grow">
+              <TeleGuidanceFrame link={list_of_links[1]}/>
             </div>
+            {/* <div className="p-10">
+              <h3 className="text-sm font-semibold text-gray-400">Integrations</h3>
+              <p className="mt-2 text-lg font-medium tracking-tight text-white">Connect your favorite tools</p>
+              <p className="mt-2 max-w-lg text-sm text-gray-400">
+                Curabitur auctor, ex quis auctor venenatis, eros arcu rhoncus massa.
+              </p>
+            </div> */}
           </div>
-          <div className="flex p-px lg:col-span-2">
-            <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 lg:rounded-bl-[2rem]">
-            <TeleGuidanceFrame />
-
-              <div className="p-10">
-                <h3 className="text-sm/4 font-semibold text-gray-400">Security</h3>
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-white">Advanced access control</p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.
-                </p>
-              </div>
+          <div className="lg:col-span-2 flex flex-col rounded-lg bg-gray-800 overflow-hidden">
+            <div className="flex-grow">
+              <TeleGuidanceFrame link={list_of_links[3]}/>
             </div>
+            {/* <div className="p-10">
+              <h3 className="text-sm font-semibold text-gray-400">Security</h3>
+              <p className="mt-2 text-lg font-medium tracking-tight text-white">Advanced access control</p>
+              <p className="mt-2 max-w-lg text-sm text-gray-400">
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.
+              </p>
+            </div> */}
           </div>
-          <div className="flex p-px lg:col-span-4">
-            <div className="overflow-hidden rounded-lg bg-gray-800 ring-1 ring-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
-              <TeleGuidanceFrame />
-              <div className="p-10">
-                <h3 className="text-sm/4 font-semibold text-gray-400">Performance</h3>
-                <p className="mt-2 text-lg/7 font-medium tracking-tight text-white">Lightning-fast builds</p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-400">
-                  Sed congue eros non finibus molestie. Vestibulum euismod augue vel commodo vulputate. Maecenas at
-                  augue sed elit dictum vulputate.
-                </p>
-              </div>
+          <div className="lg:col-span-4 flex flex-col rounded-lg bg-gray-800 overflow-hidden">
+            <div className="flex-grow">
+              <TeleGuidanceFrame link={list_of_links[4]}/>
             </div>
+            {/* <div className="p-10">
+              <h3 className="text-sm font-semibold text-gray-400">Performance</h3>
+              <p className="mt-2 text-lg font-medium tracking-tight text-white">Lightning-fast builds</p>
+              <p className="mt-2 max-w-lg text-sm text-gray-400">
+                Sed congue eros non finibus molestie. Vestibulum euismod augue vel commodo vulputate. Maecenas at
+                augue sed elit dictum vulputate.
+              </p>
+            </div> */}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
