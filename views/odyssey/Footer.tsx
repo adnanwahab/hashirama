a3b0eebf28b725ad3fd2863d360538ab116746ac
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-14 19:06:05 -0700

    simplify

--- views/odyssey/Footer.tsx
+++ views/odyssey/Footer.tsx
@@ -205,7 +205,7 @@ export default function Example() {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            Office Hours with Pau
           </p>
         </div>
       </div>
