2ce7f74465d22170a6f3f6208cd8d3fae84456ef
Adnan Wahab <shel.bernstein123@gmail.com>
2024-10-15 08:08:12 -0700

    simplify

--- js/course_handler.tsx
+++ js/course_handler.tsx
@@ -33,6 +33,7 @@ let indexHtmlContent = fs.readFileSync(filePath, "utf-8");
 
 
 
+
 //export default html;
 
 function makeReactApp() {
@@ -44,6 +45,41 @@ function makeReactApp() {
       );
       return html
 }
+
+const { spawn } = require("child_process");
+
+const containerName = "zed2i-container";
+const imageName = "<zed-container>";  // Replace with your ZED Docker image
+
+const dockerRunArgs = [
+  "run", 
+  "--rm",
+  "--runtime", "nvidia",
+  "--gpus", "all",
+  "--network", "host",
+  "--env", "DISPLAY=$DISPLAY",
+  "--volume", "/tmp/.X11-unix:/tmp/.X11-unix:rw",
+  "--device", "/dev/video0", // Adjust if using another device
+  "--name", containerName,
+  imageName,
+  "/bin/bash"
+];
+
+console.log("Starting Jetson container for ZED 2i...");
+
+const dockerProcess = spawn("docker", dockerRunArgs);
+
+dockerProcess.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+dockerProcess.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+dockerProcess.on("close", (code) => {
+  console.log(`Docker process exited with code ${code}`);
+});
 
 async function proxy(req: Request) {
    const url = new URL(req.url);
